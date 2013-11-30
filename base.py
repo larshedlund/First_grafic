3124484020b807ea4fd43e7d6dfe71568bc63e58
larshedlund <contact@larshedlund.se>
2013-11-30 21:07:16 +0100

    Patrik gnäller om Russel Crow. adding som functions in func.py

--- base.py
+++ base.py
@@ -1,14 +1,20 @@
 #IMPORTS
-import pygame, sys
+
+import pygame, sys, func
 from pygame.locals import * 
+from func import *
+#sys.path.append('/d/Projekt/python/First_grafc')
+ 
 
 pygame.init()
 #INTIALISATION
 FPS = 30 # frames per second setting
 FPSCLOCK = pygame.time.Clock()
-DISPLAY_SIZE = width, height = 1024, 768
+DISPLAY_SIZE = width, height = 640, 640
 DISPLAY = pygame.display.set_mode(DISPLAY_SIZE,0,32)
-pygame.display.set_caption('First Grafic')
+DISPLAY_SEGMENT_SIZE = 64
+DISPLAY_SEGMENT = DISPLAY_SIZE[0]/DISPLAY_SEGMENT_SIZE, DISPLAY_SIZE[1]/DISPLAY_SEGMENT_SIZE
+pygame.display.set_caption('PRICK the game')
 
 #COLORS
 BLACK = ( 0, 0, 0) 
@@ -23,67 +29,49 @@ img_left = pygame.image.load("prick_left.png")
 img_down = pygame.image.load("prick_falling.png")
 img_up = pygame.image.load("prick_flying.png")
 
-
 #TEXT
 fontObj = pygame.font.Font('freesansbold.ttf', 32)
-textSurfaceObj = fontObj.render('DAMN! Boj ar jo kool or nott', True, GREEN, BLUE)
+textSurfaceObj = fontObj.render('Everybody like to be Mr Prick', True, GREEN, BLUE)
 textRectObj = textSurfaceObj.get_rect()
-textRectObj.center = (300, 300)
+textRectObj.center = (300, 50)
 
 # CONTROLL
-direction = 'right'
+direction = 'non'
 prick_img = img_right
-prick_position_x = 50
-prick_position_y = 50
-
+prick_pos = [1,1]
+			
 # main game loop
 while True: # the main game loop
-    
-    
-    if direction == 'right':
-        prick_position_x += 5
-        if prick_position_x >= 1044:
-             prick_position_x = -30
-            
-    elif direction == 'down':
-        prick_position_y += 5
-        if prick_position_y >= 818:
-             prick_position_y = 5
-			 
-    elif direction == 'left':
-        prick_position_x -= 5
-        if prick_position_x <= -40:
-            prick_position_x = 1024
-			
-    elif direction == 'up':
-        prick_position_y -= 5
-        if prick_position_y <= -50:
-            prick_position_y = 758
-
-   
+ 
+    if direction != 'non':
+        update_prick_position(direction, prick_pos,DISPLAY_SEGMENT)
+        direction = 'non'
+		
     for event in pygame.event.get():
         if not hasattr(event, 'key'): continue
         down = event.type == KEYDOWN 
-        if event.key == K_RIGHT: 
+        if event.key == K_RIGHT or event.key ==  K_d: 
             direction = 'right'
-            prick_img = img_right
-        elif event.key == K_LEFT: 
+            PRICK_IMG = img_right
+        elif event.key == K_LEFT or event.key ==  K_a: 
             direction = 'left'
-            prick_img = img_left
-        elif event.key == K_UP: 
+            PRICK_IMG = img_left
+        elif event.key == K_UP or event.key ==  K_w: 
             direction = 'up' 
-            prick_img = img_up
-        elif event.key == K_DOWN: 
+            PRICK_IMG = img_up
+        elif event.key == K_DOWN or event.key ==  K_s: 
             direction = 'down'
-            prick_img = img_down
+            PRICK_IMG = img_down
         elif event.key == QUIT: 
-            sys.exit()
             pygame.quit()
-        elif event.key == K_ESCAPE: sys.exit(0) # quit the game
+            sys.exit()
+        elif event.key == K_ESCAPE:
+            sys.exit(0) # quit the game
 
     DISPLAY.fill(BLUE)
     DISPLAY.blit(textSurfaceObj, textRectObj)
-    DISPLAY.blit(prick_img, (prick_position_x, prick_position_y))
+    DISPLAY.blit(prick_img, (prick_dis_pos(prick_pos[0],DISPLAY_SEGMENT_SIZE),prick_dis_pos(prick_pos[1],DISPLAY_SEGMENT_SIZE)))
     
     pygame.display.update() 
     FPSCLOCK.tick(FPS)
+
--- func.py
+++ func.py
@@ -1,2 +1,28 @@
-def func1(5):
-    return 5*3
+
+
+
+def update_prick_position(new_direction, position, DISPLAY_SEGMENT):
+    if new_direction == 'right':
+        position[0] +=  1
+        if position[0] >= DISPLAY_SEGMENT[0]:
+            position[0] = 1
+            
+    elif new_direction == 'down':
+        position[1] += 1
+        if position[1] >= DISPLAY_SEGMENT[1]:
+            position[1] = 1
+			 
+    elif new_direction == 'left':
+        position[0] -= 1
+        if position[0] <= 1:
+            position[0] = DISPLAY_SEGMENT[0] - 1
+			
+    elif new_direction == 'up':
+        position[1] -= 1
+        if position[1] <= 1:
+            position[1] = DISPLAY_SEGMENT[1] - 1
+
+	return 
+	
+def prick_dis_pos(position, DISPLAY_SEGMENT_SIZE):
+    return position*DISPLAY_SEGMENT_SIZE - DISPLAY_SEGMENT_SIZE/2
